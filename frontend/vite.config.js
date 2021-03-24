dc7b3c0925989acf3e3760698eee57ffecc5d5c9
mahaveer <mahaveersharma0496@yahoo.com>
2021-03-24 23:12:23 +0530

    Add port config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), WindiCSS()],
+  server: {
+    port: 3001
+  },
 })
