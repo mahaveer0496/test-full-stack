858911f0c0da5a15127ac73708fdf83ee5c0fd92
mahaveer <mahaveersharma0496@yahoo.com>
2021-03-25 00:49:46 +0530

    Test CD

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,21 +3,34 @@ import ReactDOM from 'react-dom'
 import 'virtual:windi.css'
 import ViewArtists from './ViewArtists'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { BrowserRouter as Router, Switch, Route, Link as RouterLink } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from 'react-router-dom'
 import CreateArtist from './CreateArtist'
 
 const queryClient = new QueryClient()
 
-const Link = (props) => <RouterLink className="bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1" {...props}/>
+console.log('this is from CD!')
+
+const Link = (props) => (
+  <RouterLink
+    className='active:bg-pink-600 hover:shadow-md focus:outline-none px-4 py-2 mb-1 mr-1 text-xs font-bold text-white uppercase bg-blue-500 rounded shadow outline-none'
+    {...props}
+  />
+)
 ReactDOM.render(
   <React.StrictMode>
     <div className='container mx-auto'>
-      <QueryClientProvider client={queryClient}>        
+      <QueryClientProvider client={queryClient}>
         <Router>
-          <div className="py-4">
-          <Link to='/'>View Artists</Link> <Link to='/new'>Create Artist</Link>
+          <div className='py-4'>
+            <Link to='/'>View Artists</Link>{' '}
+            <Link to='/new'>Create Artist</Link>
           </div>
-          
+
           <Switch>
             <Route exact path='/'>
               <ViewArtists />
